2ca52b119cc1736187d7dd1c5e180c3f075dadb7
programmerNizam <mdnizam12109@gmail.com>
2024-08-25 14:58:48 +0600

    Login Form Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import NotFoundPage from './pages/NotFoundPage';
-import ProfilePage from './pages/ProfilePage';
-import RegistrationPage from './pages/RegistrationPage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
-
   return (
     <>
       <Routes>
@@ -14,11 +13,10 @@ function App() {
         <Route element={<LoginPage />} path="/login" />
         <Route element={<ProfilePage />} path="/me" />
         <Route element={<RegistrationPage />} path="/register" />
-
         <Route element={<NotFoundPage />} path="*" />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,57 @@
+import { useForm } from "react-hook-form";
+import Field from "../common/Field";
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <Field label="Email">
+        <input
+          {...register("email", { required: "Email is Required!" })}
+          className={`auth-input ${
+            errors.email ? "border-red-500" : "border-gray-200"
+          }`}
+          type="email"
+          name="email"
+          id="email"
+        />
+      </Field>
+      <Field label="Password">
+        <input
+          {...register("password", {
+            required: "Password is Required!",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
+          className={`auth-input ${
+            errors.password ? "border-red-500" : "border-gray-200"
+          }`}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Field>
+      <Field>
+        <button
+          className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
+      </Field>
+    </form>
+  );
+}
