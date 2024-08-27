764d8a3cc9b03d1525434f4cdf49a831b5409902
programmerNizam <mdnizam12109@gmail.com>
2024-08-28 00:56:34 +0600

    Post V1 completed

--- src/components/post/PostBody.jsx
+++ src/components/post/PostBody.jsx
@@ -1,5 +1,16 @@
-export default function PostBody() {
+export default function PostBody({ poster, content }) {
   return (
-    <div>PostBody</div>
-  )
+    <div className="border-b border-[#3F3F3F] py-4 lg:py-5 lg:text-xl">
+      <div className="flex items-center justify-center overflow-hidden">
+        {poster && (
+          <img
+            className="max-w-full"
+            src={`${import.meta.env.VITE_SERVER_BASE_URL}/${poster}`}
+            alt="poster"
+          />
+        )}
+      </div>
+      <p>{content ?? "No Content Available!"}</p>
+    </div>
+  );
 }
--- src/components/post/PostComments.jsx
+++ src/components/post/PostComments.jsx
@@ -1,5 +1,40 @@
-export default function PostComments() {
+import { useState } from "react";
+import { useAvatar } from "../../hooks/useAvatar";
+import PostCommentList from "./PostCommentList";
+
+export default function PostComments({ post }) {
+  const [showComment, setShowComment] = useState(false);
+  const { avatarUrl } = useAvatar(post);
   return (
-    <div>PostComments</div>
-  )
+    <div>
+      {/* <!-- comment input box --> */}
+      <div className="flex-center mb-3 gap-2 lg:gap-4">
+        <img
+          className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
+          src={avatarUrl}
+          alt="avatar"
+        />
+
+        <div className="flex-1">
+          <input
+            type="text"
+            className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
+            name="post"
+            id="post"
+            placeholder="What's on your mind?"
+          />
+        </div>
+      </div>
+      {/* <!-- comment filter button --> */}
+      <div className="mt-4">
+        <button
+          className="text-gray-300 max-md:text-sm"
+          onClick={() => setShowComment(!showComment)}
+        >
+          All Comment ▾
+        </button>
+      </div>
+      {showComment && <PostCommentList comments={post?.comments} />}
+    </div>
+  );
 }
